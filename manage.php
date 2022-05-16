5bdedff9feff22068e633f56c4cbd8dd0a6a7a05
Minh Nhat Nguyen <minhnnguyen003@gmail.com>
2022-05-16 22:52:15 +1000

    Final change in CSS and complete transform html to php

--- manage.php
+++ manage.php
@@ -1,6 +1,7 @@
 
 <?php
-    
+
 ?>
 
 $query = "insert into $sql_table (make, model, price, yom) values ('$make', '$model', '$price', '$yom')";
+select make, model, price FROM cars ORDER BY make, model
